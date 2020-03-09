14260fc5728338a7716aaef95a7b0406782c38a9
Carlos Bonadeo <carlosbonadeo@gmail.com>
2020-03-08 23:45:14 -0300

    Responsive

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,10 +8,13 @@ interface Props {
 
 const Card: FC<Props> = ({title, description, children}) => {
     return (
-        <div className="card flex-1 bg-white shadow-md rounded-md mx-2 p-3 hover:scale-105 hover:shadow-lg transform transition ease-in-out duration-75" tabIndex={0}>
+        <div 
+            className="card bg-white shadow-md rounded-md mx-2 p-3 md:hover:scale-105 hover:shadow-lg transform transition ease-in-out duration-100 flex-1 md:flex-initial md:w-64 mb-4 md:mb-0" 
+            tabIndex={0}
+        >
             <h2 className="text-2xl">{title}</h2>
             <div>{children}</div>
-            <div className="text-sm text-gray-700">{description}</div>
+            <div className="text-sm text-gray-600">{description}</div>
         </div>
     )
 }
--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -13,47 +13,47 @@ const Controls: FC = () => {
     const dispatch = useDispatch();
 
     return (
-        <div className="controls mx-auto mt-5" style={{ width: '700px' }}>
-            {/* <h1 className="text-4xl text-white text-shadow-md bold my-3">Give it a shot, cowboy!</h1> */}
-            <div className="flex text-left">
-                <Card 
-                    description="Determines how sensitive your image sensor is. Hight ISO is more sensitive to light, but adds grain."
-                >
-                    <RangeControl
-                        label="ISO"
-                        values={isoSensitivity}
-                        onChange={value => dispatch({
-                            type: SET_ISO,
-                            iso: value
-                        })}
-                    />
-                </Card>
-                <Card 
-                    description="What is in or out focus. Determines the amount of light that gets in."
-                >
-                    <RangeControl
-                        label="Aperture"
-                        values={apertureDiaphragm}
-                        onChange={value => dispatch({
-                            type: SET_APERTURE,
-                            aperture: value
-                        })}
-                    />
-                </Card>
-                <Card 
-                    description="The lenght of time when the film or digital sensor inside the camera is exposed to light."
-                >
-                    <RangeControl
-                        label="Shutter"
-                        values={shutterSpeed}
-                        onChange={value => dispatch({
-                            type: SET_SHUTTER,
-                            shutter: value
-                        })}
-                    />
-                </Card>
-            </div>
+      <div className="controls mt-5">
+        {/* <h1 className="text-4xl text-white text-shadow-md bold my-3">Give it a shot, cowboy!</h1> */}
+        <div className="flex text-left justify-center md:flex-row flex-col">
+          <Card description="Determines how sensitive your image sensor is. Hight ISO is more sensitive to light, but adds grain.">
+            <RangeControl
+              label="ISO"
+              values={isoSensitivity}
+              onChange={value =>
+                dispatch({
+                  type: SET_ISO,
+                  iso: value
+                })
+              }
+            />
+          </Card>
+          <Card description="What is in or out focus. Determines the amount of light that gets in.">
+            <RangeControl
+              label="Aperture"
+              values={apertureDiaphragm}
+              onChange={value =>
+                dispatch({
+                  type: SET_APERTURE,
+                  aperture: value
+                })
+              }
+            />
+          </Card>
+          <Card description="The lenght of time when the film or digital sensor inside the camera is exposed to light.">
+            <RangeControl
+              label="Shutter"
+              values={shutterSpeed}
+              onChange={value =>
+                dispatch({
+                  type: SET_SHUTTER,
+                  shutter: value
+                })
+              }
+            />
+          </Card>
         </div>
+      </div>
     )
 }
 
