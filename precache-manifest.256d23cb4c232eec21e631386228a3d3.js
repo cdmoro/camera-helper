ee98c8e7febdec4ec2273df081157595c8a17563
Carlos Bonadeo <carlosbonadeo@gmail.com>
2020-07-13 18:44:00 -0300

    deploy

---
+++ precache-manifest.256d23cb4c232eec21e631386228a3d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f492d6cd9f1bd67de3d5e3d17a0cce9a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6339b88151c3ee4bf5fd",
+    "url": "./static/css/main.438dbf62.chunk.css"
+  },
+  {
+    "revision": "6c111675f4849ae4596e",
+    "url": "./static/js/2.49bee8f5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.49bee8f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6339b88151c3ee4bf5fd",
+    "url": "./static/js/main.6db0c9f7.chunk.js"
+  },
+  {
+    "revision": "269c0a1eae56e48c1c40",
+    "url": "./static/js/runtime-main.e6ef0beb.js"
+  },
+  {
+    "revision": "b793e21d75aa8bb639b6ba79eebaa863",
+    "url": "./static/media/github.b793e21d.svg"
+  },
+  {
+    "revision": "9b82de57bb69132f7b81b8585a8a1cdc",
+    "url": "./static/media/noise.9b82de57.png"
+  }
+]);
